dbb8117f16836196356e66f8e97964f9ffd0fa47
cyang614 <cyang5075@gmail.com>
2024-04-20 01:04:04 +0800

    0420

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+{/*https://www.youtube.com/watch?v=miUsa5SOXGk 24:42*/}
+import {BrowserRouter as Router , Routes,Route} from "react-router-dom";
+import {Navbar} from "./components/navbar"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route path="/auth" element={<h1>Auth</h1>}></Route>
+        <Route path="/rated" element={<h1>Rated Page</h1>}></Route>
+      </Routes>
+    </Router>  
+  </div>
   )
 }
 
